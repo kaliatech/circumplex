7bc693938951146173b3bf5b3d546a57d797dc2f
Josh Sanderson <josh@jgstechnical.com>
2024-10-12 12:20:34 -0400

    Add initial project setup files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {CircumplexDemo} from "./components/CircumplexDemo";
+
+function App() {
+
+  return (
+        <CircumplexDemo />
+  )
+}
+
+export default App
---
+++ src/components/CircumplexDemo/index.tsx
@@ -0,0 +1,7 @@
+export const CircumplexDemo = () => {
+    return (
+        <div>
+        <h1>CircumplexDemo</h1>
+        </div>
+    )
+}
